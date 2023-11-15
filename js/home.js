a407b26aeee14ae4c0f62c1132263bb08b362539
Christian Panter <pantercp@outlook.com>
2023-11-15 15:07:26 +0000

    Added reviews sections

--- js/home.js
+++ js/home.js
@@ -1,32 +1,15 @@
 
-
-// // Observer callback function
-// function handleMutation(mutationsList, observer) {
-//     for (const mutation of mutationsList) {
-//       if (mutation.type === "attributes" && mutation.attributeName === "class") {
-//         const targetElement = mutation.target;
-  
-//         // Check if the "appear" class is added
-//         if (targetElement.classList.contains("appear")) {
-//           // Trigger the scrambler function
-//           scrambler(targetElement);
-          
-//           // Disconnect the observer if needed
-//           observer.disconnect();
-//         }
-//       }
-//     }
-//   }
-  
-//   // Target element to observe
-//   const targetElement = document.querySelector(".scramble");
-  
-//   // Create a MutationObserver instance
-//   const observer = new MutationObserver(handleMutation);
-  
-//   // Configure the observer to watch for changes to the 'class' attribute
-//   const observerConfig = { attributes: true };
-  
-//   // Start observing the target element
-//   observer.observe(targetElement, observerConfig);
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    grabCursor: true,
+    loop: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+  });
 
