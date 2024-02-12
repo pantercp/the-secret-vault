ec1695f9f96938c616c66edb8b1160b7e1b79315
Christian Panter <pantercp@outlook.com>
2024-02-12 10:38:05 +0000

    Added detail to policies and angela hs with autoplay carousel

--- js/about.js
+++ js/about.js
@@ -1,17 +1,21 @@
 
 var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 1,
-    grabCursor: true,
-    loop: true,
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-    },
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-    },
-  });
+  slidesPerView: 1,
+  grabCursor: true,
+  loop: true,
+  speed: 300,
+  autoplay: {
+    delay: 5000,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
 
 
 function setupToggleButtons(sectionClass) {
